ddf3a49f22365b45c980db16aeaa623139a60060
bazuzu <bazuzu@ukr.net>
2013-07-29 17:18:46 +0300

    commit 2

---
+++ markdowndsl/MarkdownDocument/MarkdownDocument.fs
@@ -0,0 +1,20 @@
+﻿namespace MarkdownDocument
+
+
+type MarkdownDocument = list<MarkdownBlock>
+
+and MarkdownBlock = 
+    | Heading of int * MarkdownSpans
+    | Paragraph of MarkdownSpans
+    | CodeBlock of list<string>
+
+and MarkdownSpans = list<MarkdownSpan>
+
+and MarkdownSpan = 
+    | Literal of string
+    | InlineCode of string
+    | String of MarkdownSpans
+    | Emphasis of MarkdownSpans
+    | HyperLink of MarkdownSpans * string
+
+
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-#Project Euler  
+#Markdown DSL
 
 Implementation of Markdown parser based on  [F# Deep Dives book](http://manning.com/petricek2/), [Chapter 3 "Parsing text based languages"](http://manning.com/petricek2/F_DeepDives_MEAP_ch3.pdf).
